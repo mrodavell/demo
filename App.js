05f88effc31358f2cc47b309992e37061a19e42a
mrodavell <mail.jomar.llevado@gmail.com>
2024-10-25 10:36:53 +0800

    demo 10 24 24

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import Profile from './src/pages/Profile';
+import Login from './src/pages/Login';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Profile />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1}}>
+          <Login />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
---
+++ src/styles/styles.js
@@ -0,0 +1,22 @@
+export const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ccc',
+    },
+    section: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input:{
+        width: '80%',
+        height: 40,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginBottom: 10
+    }
+}
