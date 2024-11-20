dfc14142d6941ccc14f4f77fb7ce1a839bb5360c
mrodavell <mail.jomar.llevado@gmail.com>
2024-11-21 07:44:36 +0800

    Fix errors and warnings, also remove drawer

--- src/app/dashboard/_layout.js
+++ src/app/dashboard/_layout.js
@@ -1,22 +1,58 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
-import DrawerContent from '../../components/Drawer';
+import React from 'react' 
+import { View } from 'react-native'
+import { Tabs } from 'expo-router' 
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const DashboarLayout = () => {
+const TabsLayout = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1}}>
-        <Drawer 
-           drawerContent={DrawerContent}    
-           screenOptions={{ 
-                headerShown: false
-           }} 
-        >
-            <Drawer.Screen name='(tabs)' options={{
-                drawerLabel: 'Home'
-            }} /> 
-        </Drawer>
-    </GestureHandlerRootView>
+    <Tabs
+        screenOptions={{
+            headerShown: false, 
+            tabBarStyle: {
+                backgroundColor: '#6650A4',
+                margin: 5,
+                padding: 5,
+                minHeight: 50,
+                borderRadius: 10,
+                paddingBottom: 5,
+            },
+            tabBarLabelStyle: {
+                fontSize: 14,
+            },
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: '#fff'
+        }}
+        initialRouteName='home'
+     > 
+        <Tabs.Screen name='index'
+            options={{
+                title: 'Home',
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ focused }) => <View>
+                    <MaterialCommunityIcons name={focused ? 'home' : 'home-outline'} color="#FFF" size={20} />
+                </View>
+            }}
+        /> 
+         <Tabs.Screen name='profile'
+            options={{
+                title: 'Profile',
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ focused }) => <View>
+                    <MaterialCommunityIcons name={focused ? 'account' : 'account-outline'} color="#FFF" size={20} />
+                </View>
+            }}
+        /> 
+         <Tabs.Screen name='settings'
+            options={{
+                title: 'Settings',
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({ focused }) => <View>
+                    <MaterialCommunityIcons name={focused ? 'cog' : 'cog-outline'} color="#FFF" size={20} />
+                </View>
+            }}
+        /> 
+     </Tabs>    
   )
 }
 
-export default DashboarLayout
+export default TabsLayout
