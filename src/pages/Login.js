ea93f76bdb06fc55ecbedef16cc3bb2be1a8e1e7
mrodavell <mail.jomar.llevado@gmail.com>
2024-11-21 08:52:50 +0800

    WIP

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput, Checkbox, Text } from 'react-native-paper';
+import { TextInput, Text } from 'react-native-paper';
 import MyButton from '../components/MyButton';
 
 const Login = () => {
     
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState(''); 
-    const [checked, setChecked] = React.useState(false);
     const [isShowPassword, setIsShowPassword] = React.useState(false);
 
   return (
     <SafeAreaView style={loginStyle.container}>
         <View style={{...loginStyle.section, alignItems: 'center', justifyContent: 'center'}}>
             <Image source={require('../assets/logo.png')} style={loginStyle.logo} />
+            <Text variant='displayMedium' style={{ marginVertical: 5}}>BossJob</Text>
         </View>
         <View style={{...loginStyle.section}}>
            <TextInput
@@ -33,21 +33,12 @@ const Login = () => {
                 secureTextEntry={!isShowPassword}
                 right={<TextInput.Icon onPress={() => setIsShowPassword(!isShowPassword)} icon={isShowPassword ? "eye": "eye-off"} />}
            />
-        </View>
-        <View style={{...loginStyle.section, flex: 0, flexDirection: 'row', alignItems: 'center'}}>
-            <Checkbox
-                status={checked ? 'checked' : 'unchecked'}
-                onPress={() => {
-                    setChecked(!checked);
-                }}
-            />
-            <Text variant='bodyLarge'>Remember Me</Text>
-        </View>
+        </View>         
         <View style={{...loginStyle.section, flex: 2}}>
-            <View style={{ margin: 5}}>
+            <View style={{ margin: 5 }}>
                 <MyButton text='Login' action={() => console.log("login")} mode='contained' size='small' />
             </View>
-            <View style={{ margin: 5}}>
+            <View style={{ margin: 5 }}>
                 <MyButton text='Register' action={() => console.log("register")} mode='contained' size='small' />
             </View>
         </View>
@@ -67,7 +58,7 @@ const loginStyle = StyleSheet.create({
     section: {
         flex:1,
         width: '100%',
-        padding: 20,
+        padding: 20, 
     },
     logo:{
         width: 200,
