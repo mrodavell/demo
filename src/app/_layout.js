0cf785c8d5a6909ac2f5a3ab509b75e5e81429e7
mrodavell <mail.jomar.llevado@gmail.com>
2024-11-21 10:22:24 +0800

    Completed Stack, Tabs, Drawer

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { PaperProvider } from 'react-native-paper'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <PaperProvider>
+        <SafeAreaProvider>
+          <Stack
+            screenOptions={{        
+              animation: 'none',
+            }}
+          >
+            <Stack.Screen name="index" options={{
+              headerShown: false
+            }} />
+            <Stack.Screen name="register" options={{ title: 'Registration'}} />
+            <Stack.Screen name="recover" options={{ title: 'Forgot Password?'}} />
+            <Stack.Screen name="dashboard" 
+              options={{ 
+                title: 'Dashboard', 
+                headerShown: false
+              }} 
+            />
+          </Stack>
+        </SafeAreaProvider>       
+    </PaperProvider>
+   
   )
 }
 
